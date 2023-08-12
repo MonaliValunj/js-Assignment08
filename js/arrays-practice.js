6bc26f1dae3f83591796344eb6454b7d0d0cdb7d
Monali Valunj <monavalunj96@gmail.com>
2023-08-11 21:06:58 -0700

    Initial commit

---
+++ js/arrays-practice.js
@@ -0,0 +1,129 @@
+//STEP 1
+/* 
+var FavMovies = ["A Beautiful Mind", "Murder mystery","Lion","The Grand Budapest Hote",
+"Australia"];
+console.log(FavMovies[1]);
+ */
+
+
+//STEP 2
+/* 
+var movies = Array(5)
+movies[0] = "A Beautiful Mind"
+movies[1] = "Murder mystery"
+movies[2] = "Lion"
+movies[3] = "The Grand Budapest Hote"
+movies[4] = "Celine"
+console.log(movies[0])
+
+ */
+
+//STEP 3
+/* 
+var movies = Array(5)
+movies[0] = "A Beautiful Mind"
+movies[1] = "Murder mystery"
+movies[2] = "Lion"
+movies[3] = "The Grand Budapest Hote"
+movies[4] = "Celine"
+
+ movies.splice(2,0,"new movie")
+ console.log(movies)
+console.log(movies.length)
+ */
+
+//STEP 4
+/* 
+var movies = [];
+movies[0] = "A Beautiful Mind"
+movies[1] = "Murder mystery"
+movies[2] = "Lion"
+movies[3] = "The Grand Budapest Hote"
+movies[4] = "Celine"
+movies.shift();
+console.log(movies);
+ */
+
+//STEP 5
+/* 
+var movies = [];
+movies[0] = "A Beautiful Mind"
+movies[1] = "Murder mystery"
+movies[2] = "Lion"
+movies[3] = "The Grand Budapest Hote"
+movies[4] = "Celine"
+movies[5] = "Australia"
+movies[6] = "Good Night Oppy"
+
+for (let i in movies) {
+	console.log(movies[i]);
+}
+
+ */
+
+//STEP 6
+/* 
+var movies = [];
+movies[0] = "A Beautiful Mind"
+movies[1] = "Murder mystery"
+movies[2] = "Lion"
+movies[3] = "The Grand Budapest Hote"
+movies[4] = "Celine"
+movies[5] = "Australia"
+movies[6] = "Good Night Oppy"
+
+for (let i of movies) {
+	console.log(i);
+}
+ */
+//STEP 7
+
+/* var movies = [];
+movies[0] = "A Beautiful Mind"
+movies[1] = "Murder mystery"
+movies[2] = "Lion"
+movies[3] = "The Grand Budapest Hote"
+movies[4] = "Celine"
+movies[5] = "Australia"
+movies[6] = "Good Night Oppy"
+
+for (let i of movies) {
+	console.log(movies.sort(i));
+     ;
+} */
+
+//STEP 8
+var movies = [];
+movies[0] = "A Beautiful Mind"
+movies[1] = "Murder mystery"
+movies[2] = "Lion"
+movies[3] = "Celine"
+movies[4] = "Good Night Oppy"
+
+var leastFavMovies = ["Drishaym2","Crazy Rich Asians","All Good Things"]
+
+console.log("Movies I liked: ")
+for(var i = 0; i < movies.length; i++){
+     console.log(movies[i])
+}
+
+console.log("Movies I regret watching: ")
+
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
